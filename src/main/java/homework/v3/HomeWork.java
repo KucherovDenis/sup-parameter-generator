b300ec5990f120e61071edb9b28a50e2d781ddda
KucherovDenis <denku-87@mail.ru>
2019-11-07 00:08:28 +0300

    Домашнее задание

---
+++ src/main/java/homework/v3/HomeWork.java
@@ -0,0 +1,66 @@
+package homework.v3;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import homework.v3.entity.JsonFileClass;
+
+
+import java.io.*;
+
+/**
+ * Задание
+ * 1) Составить файл с JSON-объектом, который "разложен" в пакете homework.v3.entity.
+ * Определить какой элемент является корневым
+ * Дать имя файлу homework.parameters.json
+ * 2) Заполнить его значениями (как пример "parameters.v1.json")
+ * 3) Считать получившийся homework.parameters.json в память
+ * 4) Сериализовать его с помощью встроенного механиза сериализации в файл с именем homework.parameters.ser
+ * 5) Сериализовать его с использованием интерфейса Externalizable в файл с именем homework.parameters.exter
+ * 6) Считать данные из файла homework.parameters.ser в память и сохранить в формате JSON в файл с именем homework.result.ser.parameters.json
+ * 7) Считать данные из файла homework.parameters.exter в память и сохранить в формате JSON в файл с именем homework.result.exter.parameters.json
+ * 8) Убедиться, что файлы homework.result.ser.parameters.json и  homework.result.exter.parameters.json
+ * совпадают с homework.parameters.json
+ */
+public class HomeWork {
+
+    private static final String JSON_FILE_NAME = "homework.parameters.json";
+    private static final String BASE_SERIALIZE_FILE_NAME = "homework.parameters.ser";
+
+    public static void writeJson(File url, JsonFileClass clazz) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(url, clazz);
+    }
+
+    public static JsonFileClass readJson(File url) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        final JsonFileClass fileClass = mapper.readValue(url, JsonFileClass.class);
+        return fileClass;
+    }
+
+    public static <T extends Serializable> void baseSerialize(String fileName, T clazz) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             ObjectOutputStream out = new ObjectOutputStream(fos);) {
+            out.writeObject(clazz);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(JSON_FILE_NAME);
+        //шаг 1 и 2
+        if (!file.exists()) {
+            JsonFileClass jsonFile = JsonFileCreator.getInstance();
+            System.out.println("Записываем данные в файла " + JSON_FILE_NAME);
+            writeJson(file, jsonFile);
+            System.out.println("Данные записаны на диск в файл " + JSON_FILE_NAME);
+        }
+
+        //шаг 3
+        System.out.println("Чтение данных из файла " + JSON_FILE_NAME);
+        JsonFileClass jsonFile = readJson(file);
+        System.out.println("Данные считаны\n" + jsonFile.toString());
+        //шаг 4
+        System.out.println("Записываем данные в файла " + BASE_SERIALIZE_FILE_NAME);
+        baseSerialize(BASE_SERIALIZE_FILE_NAME, jsonFile);
+        System.out.println("Данные записаны на диск в файл " + BASE_SERIALIZE_FILE_NAME);
+    }
+}
---
+++ src/main/java/homework/v3/JsonFileCreator.java
@@ -0,0 +1,95 @@
+package homework.v3;
+
+import homework.v3.entity.Bundle;
+import homework.v3.entity.JsonFileClass;
+import homework.v3.entity.JsonParameters;
+import homework.v3.entity.Path;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonFileCreator {
+    private static Map<String, Integer> names = new HashMap<>();
+
+    private static String createString(String name) {
+        int stringId = names.getOrDefault(name, 0);
+        String result = name + (++stringId);
+        names.put(name, stringId);
+        return result;
+    }
+
+    private static void setRoles(JsonParameters param, int count) {
+        List<String> roles = new ArrayList<>();
+        param.setRoles(roles);
+        for (int i = 0; i < count; i++) {
+            roles.add(createString("role"));
+        }
+    }
+
+    private static Path createPath() {
+        Path path = new Path();
+        path.setCode(createString("code"));
+        path.setValue(createString("value"));
+        return path;
+    }
+
+    private static void setPaths(Bundle bundle, int count) {
+        List<Path> paths = new ArrayList<>();
+        bundle.setPath(paths);
+        for(int i = 0; i< count; i++) {
+            paths.add(createPath());
+        }
+    }
+
+    private static void setValues(Bundle bundle, int count) {
+        List<String> values = new ArrayList<>();
+        bundle.setValues(values);
+        for (int i = 0; i < count; i++) {
+            values.add(createString("value"));
+        }
+    }
+
+    private static Bundle createBundle() {
+        Bundle bundle = new Bundle();
+        setValues(bundle, 2);
+        setPaths(bundle, 2);
+        return bundle;
+    }
+
+    private static void setBundles(JsonParameters param, int count) {
+        List<Bundle> bundles = new ArrayList<>();
+        param.setBundle(bundles);
+        for (int i = 0; i < count; i++) {
+            bundles.add(createBundle());
+        }
+    }
+
+    private static JsonParameters createParam() {
+        JsonParameters param = new JsonParameters();
+        param.setName(createString("name"));
+        param.setDescription(createString("description"));
+        param.setType(createString("type"));
+        param.setList(names.size() % 2 == 0);
+
+        setRoles(param, 2);
+        setBundles(param, 2);
+        return param;
+    }
+
+    private static void setParams(JsonFileClass jsonFile, int count) {
+        List<JsonParameters> params = new ArrayList<>();
+        jsonFile.setParameters(params);
+        for (int i = 0; i < count; i++) {
+            params.add(createParam());
+        }
+    }
+
+    public static JsonFileClass getInstance() {
+        JsonFileClass jsonFile = new JsonFileClass();
+        jsonFile.setVersion("1.0");
+        setParams(jsonFile, 2);
+        return jsonFile;
+    }
+}
